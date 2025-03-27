0700820b0af598d05a418657d175c30cbc612871
Aerrow <businessasimk@gmail.com>
2025-03-27 13:29:33 +0100

    populate rolled back

--- src/main/java/dat/config/Populate.java
+++ src/main/java/dat/config/Populate.java
@@ -18,20 +18,21 @@ public class Populate {
         try {
             tx.begin();
 
-            Game game1 = new Game("Counter Strike 2");
+            Game game1 = new Game("Counter Strike");
             em.persist(game1);
 
-            Strategy strategy1 = new Strategy("Mid fake", "Two terrorists push mid with a flash. The rest wait at B doors until attention is drawn mid, then rush up B ramp.", false, StrategyType.SERIOUS);
-            Strategy strategy2 = new Strategy("B split (Cave push)", "Four terrorists push through cave. One waits at ramp. All crunch B site at the same time.", false, StrategyType.AVERAGE);
-
+            Strategy strategy1 = new Strategy("Mid fake", "2 terrorist push mid with flash, the rest waits in b doors until attention is mid, then rush up b ramp.", false, StrategyType.SERIOUS);
+            Strategy strategy2 = new Strategy("B split(Cave push)", "First 4 terrorist push cave, 1 waits ramp for them to crunch 'B' site at the same time.", false, StrategyType.AVERAGE);
             em.persist(strategy1);
             em.persist(strategy2);
 
             Map map1 = new Map("Ancient", game1);
             Map map2 = new Map("Inferno", game1);
-
             em.persist(map1);
             em.persist(map2);
+
+            map1.getStrategies().add(strategy1);
+            map1.getStrategies().add(strategy2);
 
             strategy1.getMaps().add(map1);
             strategy2.getMaps().add(map1);
@@ -40,13 +41,17 @@ public class Populate {
             em.persist(gun1);
 
             tx.commit();
-            System.out.println("Populated database via HTTP request");
+
+            System.out.println("Database populated with sample data.");
 
         } catch (RuntimeException e) {
-            if (tx.isActive()) tx.rollback();
+            if (tx.isActive()) {
+                tx.rollback();
+            }
             e.printStackTrace();
         } finally {
             em.close();
+            emf.close();
         }
     }
 }
